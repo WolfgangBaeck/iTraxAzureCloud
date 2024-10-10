9485bc21b0e5f8c66687f7a5407b093d7e871a7a
Wolfgang Baeck <wbaeck@earthlink.net>
2024-10-10 16:31:03 -0600

    using replace for backend setup

--- provider.tf
+++ provider.tf
@@ -3,15 +3,11 @@ terraform {
   required_version = ">= 1.5.7, < 2.0.0"
 
   backend "azurerm" {
-    # resource_group_name  = "#{RGN}#" 
-    # storage_account_name = "#{SAN}#" 
-    # container_name       = "#{CN}#" 
-    # subscription_id      = "#{SUB}#"
-    resource_group_name  = "rg-tfstate" 
-    storage_account_name = "stohopeandhometfstates" 
-    container_name       = "cont-hah-tfstate" 
-    key                  = "hah"
-    subscription_id      = "c056d029-5e26-4859-b02c-35e4714819c7"
+    resource_group_name  = "#{RGN}#" 
+    storage_account_name = "#{SAN}#" 
+    container_name       = "#{CN}#" 
+    subscription_id      = "#{SUB}#"
+    key                  = "#{KEY}#"
   }
 
   required_providers {
