66f107b3ddb6d016a3f640b4a66c38a0cea4493c
Wolfgang Baeck <wbaeck@earthlink.net>
2024-10-11 09:06:22 -0600

    testing using sql admin group

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
-data "azuread_user" "wolfgang_baeck" {
-  user_principal_name = "Wolfgang@hopeandhome.org"
+data "azuread_group" "sqlserveradmin" {
+  display_name = "iTrax - SQL Admins"
 }
 
 resource "azurerm_resource_group" "prod_rg" {
@@ -10,31 +10,31 @@ resource "azurerm_resource_group" "prod_rg" {
 }
 
 # Azure SQL Server
-# resource "azurerm_mssql_server" "azuresqlserver" {
-#   name                = "${var.client_name}-itrax-sqlserver-prod"
-#   resource_group_name = azurerm_resource_group.prod_rg.name
-#   location            = azurerm_resource_group.prod_rg.location
-#   version             = "12.0"
-#   minimum_tls_version = "1.2"
+resource "azurerm_mssql_server" "azuresqlserver" {
+  name                = "${var.client_name}-itrax-sqlserver-prod"
+  resource_group_name = azurerm_resource_group.prod_rg.name
+  location            = azurerm_resource_group.prod_rg.location
+  version             = "12.0"
+  minimum_tls_version = "1.2"
 
-#   azuread_administrator {
-#     login_username              = data.azuread_user.wolfgang_baeck.user_principal_name
-#     object_id                   = data.azuread_user.wolfgang_baeck.object_id
-#     tenant_id                   = data.azurerm_client_config.current.tenant_id
-#     azuread_authentication_only = true # Enforce Azure Entra-only authentication
-#   }
-# }
+  azuread_administrator {
+    login_username              = data.azuread_group.sqlserveradmin.display_name
+    object_id                   = data.azuread_group.sqlserveradmin.object_id
+    tenant_id                   = data.azurerm_client_config.current.tenant_id
+    azuread_authentication_only = true # Enforce Azure Entra-only authentication
+  }
+}
 
 # SQL Database
-# resource "azurerm_mssql_database" "sql_db" {
-#   name         = "iTraxBlazer_db"
-#   server_id    = azurerm_mssql_server.azuresqlserver.id
-#   collation    = "SQL_Latin1_General_CP1_CI_AS" # Specify the collation
-#   license_type = "LicenseIncluded"              # License configuration
-#   max_size_gb  = 400                            # Specify database size
-#   sku_name     = "GP_Gen5_2"                    # Database SKU (e.g., S0, S1, etc.)
-#   enclave_type = "VBS"                          # Optional enclave setting
-# }
+resource "azurerm_mssql_database" "sql_db" {
+  name         = "iTraxBlazer_db"
+  server_id    = azurerm_mssql_server.azuresqlserver.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS" # Specify the collation
+  license_type = "LicenseIncluded"              # License configuration
+  max_size_gb  = 400                            # Specify database size
+  sku_name     = "GP_Gen5_2"                    # Database SKU (e.g., S0, S1, etc.)
+  enclave_type = "VBS"                          # Optional enclave setting
+}
 
 # Service Plan
 resource "azurerm_service_plan" "service_plan" {
