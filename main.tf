57e15f1f18e133001e462525d06c033deb02d926
Wolfgang Baeck <wbaeck@earthlink.net>
2024-10-18 15:12:59 -0600

    wrong abbr. for P0v3

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "azurerm_service_plan" "service_plan" {
   location            = azurerm_resource_group.prod_rg.location
   resource_group_name = azurerm_resource_group.prod_rg.name
   os_type             = "Windows"
-  sku_name            = "P0V3"
+  sku_name            = "P0v3"
 }
 
 # Windows Web App
